1f14038fa940cbc6f19aa62164f6788a950a09af
Matthew Exon <git.mexon@spamgourmet.com>
2013-04-07 08:52:05 +0800

    Starting to figure out how to hook this up to the email system

--- postbymail/mail_handler.php
+++ postbymail/mail_handler.php
@@ -1,11 +1,9 @@
 #!/usr/bin/php
 <?php
 
-chdir(dirname(__file__) . "/..");
-
 require_once("boot.php");
 
-function incoming_mail_hooks_run($argv, $argc){
+function postbymail_hooks_run($argv, $argc){
     global $a, $db;
 
     if(is_null($a)) {
@@ -29,17 +27,31 @@ function incoming_mail_hooks_run($argv, $argc){
 
     load_hooks();
 
-    logger('incoming_mail: start');
+    logger('postbymail: start');
 
-    $d = datetime_convert();
+    postbymail_decode_stdin();
 
-    call_hooks('incoming_mail', $d);
+//    call_hooks('postbymail', $d);
 
     return;
 }
 
+function postbymail_decode_stdin() {
+    require_once 'Mail/mimeDecode.php';
+    require_once('include/html2bbcode.php');
+
+    $content = file_get_contents("php://stdin");
+
+    $decoder = new Mail_mimeDecode($content);
+    $structure = $decoder->decode(array('include_bodies' => TRUE, 'decode_bodies' => TRUE, 'decode_headers' => TRUE));
+
+    $from = $message['from'];
+    logger('postbymail_decode_stdin: got message from ' . $message['from'] . ' subject ' . $message['subject']);
+
+}
+
 if (array_search(__file__,get_included_files())===0){
-    incoming_mail_hooks_run($argv,$argc);
+    postbymail_hooks_run($argv,$argc);
     killme();
 }
 
