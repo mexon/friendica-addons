14f64f558511298f2a724e011eb732e4b22877c8
Matthew Exon <github.mexon@spamgourmet.com>
2013-08-05 04:53:37 +0200

    Fix bad call to fetch_url

--- retriever/retriever.php
+++ retriever/retriever.php
@@ -223,7 +223,8 @@ function retrieve_resource($resource) {
            ' attempt at resource ' . $resource['id'] . ' ' . $resource['url'], LOGGER_DEBUG);
     q("UPDATE `retriever_resource` SET `last-try` = now(), `num-tries` = `num-tries` + 1 WHERE id = %d",
       intval($resource['id']));
-    $data = fetch_url($resource['url'], $resource['binary'], 0, 0, Null, '/tmp/friendica/cookies.txt');
+    $redirects;
+    $data = fetch_url($resource['url'], $resource['binary'], $redirects, 0, Null, '/tmp/friendica-cookies.txt');
     $resource['type'] = get_app()->get_curl_content_type();
     if ($data) {
         $resource['data'] = $data;
