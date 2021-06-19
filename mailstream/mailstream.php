933b26d927538aef7570f401bf7bf79dd1bc7cca
Matthew Exon <git.mexon@spamgourmet.com>
2021-06-19 19:22:37 +0200

    sync with submitted

--- mailstream/mailstream.php
+++ mailstream/mailstream.php
@@ -316,11 +316,6 @@ function mailstream_subject($item)
 		return DI::l10n()->t("Friendica post");
 	}
 	$contact = $r[0];
-	if (!DBA::isResult($r)) {
-		Logger::error('mailstream_subject no contact for item id ' . $item['id'] . ' plink ' . $item['plink'] . ' contact id ' . $item['contact-id'] . ' uid ' . $item['uid']);
-		return DI::l10n()->t("Friendica post");
-	}
-	$contact = $r[0];
 	if ($contact['network'] === 'dfrn') {
 		return DI::l10n()->t("Friendica post");
 	}
